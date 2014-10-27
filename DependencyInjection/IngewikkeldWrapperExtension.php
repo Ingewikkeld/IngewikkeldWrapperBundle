e2180a7388ea05cd7e860cf235eb920eeecf51df
George Zankevich <gzankevich@gmail.com>
2014-10-27 17:58:32 +0000

    service Changed to yml for service config to prevent brain damage

--- DependencyInjection/IngewikkeldWrapperExtension.php
+++ DependencyInjection/IngewikkeldWrapperExtension.php
@@ -23,6 +23,6 @@ class IngewikkeldWrapperExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader->load('services.yml');
     }
 }
